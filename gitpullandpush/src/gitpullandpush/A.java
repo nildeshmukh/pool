da41a9c6d1d7344b5db8ca88d59f4c7ade1799f4
Nilesh <Nilesh@DESKTOP-EVJM375>
2022-04-25 12:44:33 +0530

    added new m4 method

--- gitpullandpush/src/gitpullandpush/A.java
+++ gitpullandpush/src/gitpullandpush/A.java
@@ -7,16 +7,24 @@ public class A {
 	{
 		System.out.println("This m1 method");
 	}
-
+	
+//this pool added method
 	public void m2()
 	{
 		System.out.println("This m2 method");
+		
 	}
+	
 	public static void main(String[] args) 
 	{
 		
 		System.out.println("This is main method");
 
+	}
+	
+	public void m4()
+	{
+		System.out.println("This m2 method");
 	}
 
 }
