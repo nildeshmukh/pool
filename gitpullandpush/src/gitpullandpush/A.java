3c3491506fd9de06e0daff145fc8d891daa50d74
Nilesh <Nilesh@DESKTOP-EVJM375>
2022-04-25 12:32:23 +0530

    it is pool concept

---
+++ gitpullandpush/src/gitpullandpush/A.java
@@ -0,0 +1,18 @@
+package gitpullandpush;
+
+public class A {
+	
+	
+	public void m1()
+	{
+		System.out.println("This m1 method");
+	}
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("This is main method");
+
+	}
+
+}
